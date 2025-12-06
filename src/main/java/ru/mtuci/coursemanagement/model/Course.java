4c20a04c06c293c8558119087d48350491dfc0a3
Nastya Kotrovskaya <kotro_nastya@bk.ru>
2025-12-06 12:40:48 +0300

    courses module (Table 3)

--- src/main/java/ru/mtuci/coursemanagement/model/Course.java
+++ src/main/java/ru/mtuci/coursemanagement/model/Course.java
@@ -1,29 +1,21 @@
 package ru.mtuci.coursemanagement.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "courses")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String title;
+    private String name;
 
-    @Column(length = 2000)
-    private String description;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    @Column
-    private Long teacherId;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
