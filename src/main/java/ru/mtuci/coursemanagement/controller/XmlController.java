a4ea94ae071b184003a088d473055ec7bfbcca26
Nastya Kotrovskaya <kotro_nastya@bk.ru>
2025-12-27 00:45:27 +0300

    fix: защита от XXE атак в XML парсере

--- src/main/java/ru/mtuci/coursemanagement/controller/XmlController.java
+++ src/main/java/ru/mtuci/coursemanagement/controller/XmlController.java
@@ -6,7 +6,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.SAXParserFactory;
 import java.io.StringReader;
 
 @RestController
@@ -14,6 +17,14 @@ public class XmlController {
     @PostMapping(value = "/api/xml/parse", consumes = {MediaType.TEXT_XML_VALUE, MediaType.APPLICATION_XML_VALUE})
     public String parse(@RequestBody String xml) throws Exception {
         SAXReader reader = new SAXReader();
+        
+        // Защита от XXE атак
+        // Отключаем обработку внешних сущностей
+        reader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        reader.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        reader.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        reader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        
         Document doc = reader.read(new StringReader(xml));
         return doc.getRootElement().getText();
     }
